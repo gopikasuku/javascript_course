7fa9fdc5fbc2fdbe5ba85613bfd7b642e758a8fb
gopikasuku <gopikasuku@gmail.com>
2024-07-02 10:55:28 -0400

    clear all added

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -2,6 +2,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAll = document.getElementById("clearAll");
 let tasks = [];
 function addTask() {
             const taskText = taskInput.value.trim();
@@ -29,6 +30,11 @@ function toggleTask(index) {
             tasks = tasks.filter(task => !task.completed);
             displayTasks();
         }
+        function clearAllTasks(){
+            tasks = [];
+            displayTasks();
+        }
         addTaskBtn.addEventListener('click', addTask);
 clearCompletedBtn.addEventListener('click', clearCompletedTasks);
+clearAll.addEventListener('click', clearAllTasks);
 displayTasks();
